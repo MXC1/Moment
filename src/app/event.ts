83759e11125bdb41c457cd191d47eaa7fe2105cf
MXC1 <mfhcor@gmail.com>
2020-02-12 12:56:36 +0000

    Dummy Header Images for Events

--- src/app/event.ts
+++ src/app/event.ts
@@ -2,8 +2,13 @@ export class EventContent {
 
     postIds: string[];
     followerIds: string[];
+    headerImage: string;
 
     constructor(public id: string,
                 public name: string,
-                public creatorId: string) { }
+                public creatorId: string) {
+        this.postIds = [];
+        this.followerIds = [];
+        this.headerImage = 'https://www.dailydot.com/wp-content/uploads/2019/07/netflix-family-reunion-show.jpg';
+    }
 }
