14e16b7f460f7dc64f0a1bca8135be843ebbe4ec
MXC1 <mfhcor@gmail.com>
2020-04-27 14:50:23 +0100

    Merged Event Discover and Search Pages

--- src/app/tabs/events/event-discover/event-discover.component.ts
+++ src/app/tabs/events/event-discover/event-discover.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { EventContent } from 'src/app/shared/models/event';
 import { Subscription } from 'rxjs';
@@ -17,24 +17,27 @@ import { E2BIG } from 'constants';
 })
 export class EventDiscoverComponent implements OnInit {
 
-  loadedEvents: EventContent[];
+  followedEvents: EventContent[];
   displayedEvents: { event: EventContent, weight: number }[] = [];
+  loadedEvents: { event: EventContent, weight: number }[] = [];
   private eventsSubscription: Subscription;
   isLoading = false;
+
+  @ViewChild('searchbar', { static: false }) searchbar;
 
   constructor(private modalController: ModalController, private eventsService: EventsService, private authService: AuthService, private usersService: UsersService) { }
 
   ngOnInit() {
     this.isLoading = true;
-    this.fetchPopularEvents();
+    this.fetchAllEvents();
   }
 
   fetchTailoredEvents() {
     this.isLoading = true;
-    this.displayedEvents = [];
+    this.loadedEvents = [];
     this.authService.getUserId.pipe(take(1)).subscribe(userId => {
       this.eventsSubscription = this.eventsService.fetchEvents().subscribe(allEvents => {
-        this.loadedEvents = allEvents.filter(e => e.followerIds.some(i => i === userId));
+        this.followedEvents = allEvents.filter(e => e.followerIds.some(i => i === userId));
 
         // Return each event followed by someone who is also a follower of an event I follow
         // e = every event
@@ -43,22 +46,25 @@ export class EventDiscoverComponent implements OnInit {
         // lef = every follower of every event I follow
         allEvents.filter(e =>
           e.followerIds.some(f =>
-            this.loadedEvents.some(le =>
+            this.followedEvents.some(le =>
               le.followerIds.some(lef =>
                 lef === f))) && !e.isPrivate)
           .forEach(event => {
-            if (!this.displayedEvents.some(e => e.event.id === event.id)) {
-              if (!this.loadedEvents.some(e => e.id === event.id)) {
-                this.displayedEvents = this.displayedEvents.concat({ event: event, weight: 1 });
+            if (!this.loadedEvents.some(e => e.event.id === event.id)) {
+              if (!this.followedEvents.some(e => e.id === event.id)) {
+                this.loadedEvents = this.loadedEvents.concat({ event: event, weight: 1 });
               }
             } else {
-              this.displayedEvents.find(e => e.event.id === event.id).weight++;
+              this.loadedEvents.find(e => e.event.id === event.id).weight++;
             }
           });
 
-        this.displayedEvents = this.displayedEvents.sort((e1, e2) => {
+        this.loadedEvents = this.loadedEvents.sort((e1, e2) => {
           return e2.weight - e1.weight;
         });
+
+        this.displayedEvents = this.loadedEvents;
+
         this.isLoading = false;
       });
 
@@ -68,30 +74,68 @@ export class EventDiscoverComponent implements OnInit {
   fetchPopularEvents() {
     this.isLoading = true;
 
-    this.displayedEvents = [];
+    this.loadedEvents = [];
 
     this.authService.getUserId.pipe(take(1)).subscribe(userId => {
       this.eventsService.fetchEvents().pipe(take(1)).subscribe(allEvents => {
 
-        this.loadedEvents = allEvents.filter(e => e.followerIds.some(i => i === userId));
+        // Events already being followed
+        this.followedEvents = allEvents.filter(e => e.followerIds.some(i => i === userId));
 
-        this.displayedEvents = allEvents.filter(e => !e.isPrivate
-          && !this.loadedEvents.some(le => le.id === e.id))
+        this.loadedEvents = allEvents.filter(e => !e.isPrivate
+          && !this.followedEvents.some(le => le.id === e.id))
           .sort((e1, e2) => e2.followerIds.length - e1.followerIds.length)
           .map(e => {
             return { event: e, weight: 1 };
           });
+
+          this.displayedEvents = this.loadedEvents;
 
         this.isLoading = false;
       });
     });
   }
 
+  fetchAllEvents() {
+    this.isLoading = true;
+
+    this.loadedEvents = [];
+
+    this.authService.getUserId.pipe(take(1)).subscribe(userId => {
+      this.eventsService.fetchEvents().pipe(take(1)).subscribe(allEvents => {
+
+        // Events already followed
+        this.followedEvents = allEvents.filter(e => e.followerIds.some(i => i === userId));
+
+        this.loadedEvents = allEvents.filter(e => !e.isPrivate
+          && !this.followedEvents.some(le => le.id === e.id))
+          .reverse()
+          .map(e => {
+            return { event: e, weight: 1 };
+          });
+
+          this.displayedEvents = this.loadedEvents;
+
+        this.isLoading = false;
+      });
+    });
+  }
+
+  onSearch(event) {
+    const searchValue = event.srcElement.value.toLowerCase();
+    
+    this.displayedEvents = this.loadedEvents.filter(e => e.event.name.includes(searchValue) || e.event.location.includes(searchValue));
+  }
+
   onChangeSegment(event: CustomEvent<SegmentChangeEventDetail>) {
+    this.searchbar.value = "";
+
     if (event.detail.value === "popular") {
       this.fetchPopularEvents();
-    } else {
+    } else if (event.detail.value === "tailored") {
       this.fetchTailoredEvents();
+    } else if (event.detail.value === "all") {
+      this.fetchAllEvents();
     }
   }
 
